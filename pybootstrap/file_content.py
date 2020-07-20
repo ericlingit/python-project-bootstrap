ab613f5c7d86f756b2b734bfa13908778e92f2a0
ericlingit <ericlingit@users.noreply.github.com>
2020-07-20 11:27:12 +0900

    Add content funcs

--- pybootstrap/file_content.py
+++ pybootstrap/file_content.py
@@ -1,3 +1,38 @@
-README = """# """
+def setup_content(name, author, email, desc, url):
+    return f"""import setuptools
 
-SETUP = """"""
+with open("README.md") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="{name}",
+    version="0.0.1",
+    author="{author}",
+    author_email="{emai}",
+    description="{desc}",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="{url}",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+)"""
+
+
+def testpy_content(src_dir):
+    return f"""import unittest
+from {src_dir} import app
+
+class TestAddFunc(unittest.TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+    def test_app(self):
+        self.assertTrue(True)
+
+
+if __name__ == "__main__":
+    unittest.main()"""
