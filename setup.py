65561d53a5f0f358d4d39b5ee943b9630f39a15e
ericlingit <ericlingit@users.noreply.github.com>
2020-07-24 10:48:02 +0900

    Update setup.py info

--- setup.py
+++ setup.py
@@ -7,14 +7,17 @@ setuptools.setup(
     name="pybootstrap-ericlingit",
     version="0.0.1",
     author="ericlingit",
+    author_email="ericlingit@users.noreply.github.com",
     description="A python project folder bootstrapper",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    url="https://github.com/ericlingit/python-project-bootstrap",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: POSIX :: Linux",
+        "Topic :: Software Development",
     ],
     python_requires='>=3.6',
 )
